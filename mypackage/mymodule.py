7012bca119bddfadc607cd086a88367ec11feb4e
CarterL_Day <yuk7du@virginia.edu>
2024-07-02 11:15:07 -0400

    adding the package and the setup to github

---
+++ mypackage/mymodule.py
@@ -0,0 +1,3 @@
+def favorite_movie():
+	print("My favorite movie is Interstellar.")
+
---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name = 'M09Exercise', version = '0.1', description = 'Demonstration package', url = "https://github.com/cartday8/M09Exercise", author = "Carter Day", author_email = 'yuk7du@virginia.edu',license = 'MIT', packages = ['mypackage'])
